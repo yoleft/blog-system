ce1d0634b5c6a45142af5551ec72263a934ac153
Yoleft <simon0232@gmail.com>
2018-03-10 00:39:08 +0800

    First Commit

---
+++ routes/apis.js
@@ -0,0 +1,85 @@
+require('../lib/db');
+var express = require('express');
+var router = express.Router();
+var mongoose = require('mongoose');
+var Blog = mongoose.model('Blog');
+var Comment = mongoose.model('Comment');
+
+/* GET users listing. */
+router.post('/login', function(req, res, next) {
+  if((!req.body.user) || (!req.body.passwd)){
+    res.redirect('/users/register');
+    return;
+  }
+  req.session.name = req.body.user;
+  req.session.passwd = req.body.passwd;
+  req.session.logined = true;
+  res.redirect('/');
+});
+
+router.post('/add', function(req, res, next){
+  if(!req.session.name){
+    res.redirect('/');
+    return;
+  }
+  new Blog({
+    Username: req.session.name,
+    Article: req.body.Content,
+    CreateDate: Date.now()
+  }).save(function(err){
+    if(err){
+      console.log('fail to save to DB');
+      return;
+    }
+    console.log('save to db');
+  });
+  res.redirect('/');
+});
+
+router.get('/delete/:id', function(req, res, next){
+  Blog.remove({_id: req.params.id}, function(err){
+    if(err){
+      console.log('fail to delete article');
+    }else{
+      console.log('Done');
+    }
+  });
+  res.redirect('/users/profile');
+});
+
+router.post('/update/:id', function(req, res, next){
+  if(!req.params.id){
+    res.redirect('/');
+    return;
+  }
+  Blog.update({_id: req.params.id}, { Article: req.body.Content}, function(err){
+    if(err){
+      console.log('fail to update article');
+    }else{
+      console.log('done');
+    }
+  })
+  res.redirect('/');
+});
+
+router.post('/comment/:id', function(req, res, next){
+  if(!req.params.id){
+    res.redirect('/');
+    return;
+  }
+  new Comment({
+    Visitor: req.body.Visitor,
+    Comment: req.body.Comment,
+    MessageID: req.params.id,
+    CreateDate: Date.now()
+  }).save(function(err){
+    if(err){
+      console.log('fail to save to db');
+    }else{
+      console.log('save to db');
+    }
+  });
+  res.redirect('/users/message/' + req.params.id);
+});
+
+module.exports = router;
---
+++ routes/users.js
@@ -0,0 +1,91 @@
+require('../lib/db');
+var express = require('express');
+var router = express.Router();
+var mongoose = require('mongoose');
+var Blog = mongoose.model('Blog');
+var Comment = mongoose.model('Comment');
+
+/* GET users listing. */
+router.get('/register', function(req, res, next) {
+  if(req.session.logined){
+    res.redirect('/');
+    return;
+  }
+  res.render('users/register', {title: 'Register'});
+});
+
+router.get('/signin', function(req, res, next) {
+  if(req.session.logined){
+    res.redirect('/');
+    return;
+  }
+  res.render('users/login');
+});
+
+router.get('/signout', function(req, res, next) {
+  req.session.logined = false;
+  res.redirect('/');
+  res.end();
+});
+
+router.get('/forget', function(req, res, next) {
+  if(req.session.logined){
+    res.redirect('/');
+    return;
+  }
+  res.render('users/forget');
+});
+
+router.get('/profile', function(req, res, next) {
+  if((!req.session.name) || (!req.session.logined)){
+    res.redirect('/');
+    return;
+  }
+  res.locals.username = req.session.name;
+  Blog.find({ Username: req.session.name }, function(err, blogs, count){
+    res.render('users/profile', {
+      title : 'Blog System',
+      blogs: blogs
+    });
+  });
+});
+
+router.get('/add_article', function(req, res, next) {
+  if((!req.session.name) || (!req.session.logined)){
+    res.redirect('/');
+    return;
+  }
+  res.locals.username = req.session.name;
+  res.render('users/add_article');
+});
+
+router.get('/modify/:id', function(req, res, next) {
+  if((!req.session.name) || (!req.session.logined)){
+    res.redirect('/');
+    return;
+  }
+  res.locals.username = req.session.name;
+  res.locals.messageID = req.params.id;
+  Blog.find({_id: req.params.id}, function(err, blogs, count){
+    res.render('users/modify',{
+      blogs: blogs
+    });
+  });
+});
+
+router.get('/message/:id', function(req, res, next) {
+  res.locals.username = req.session.name;
+  res.locals.authenticated = req.session.logined;
+  res.locals.messageID = req.params.id;
+  Blog.find({_id: req.params.id}, function(err, blogs, count){
+    Comment.find({MessageID: req.params.id}, function(err, comments, count){
+      res.render('users/message', {
+        title: 'Blog System',
+        blogs: blogs,
+        comments: comments
+      })
+    })
+  });
+});
+
+module.exports = router;
